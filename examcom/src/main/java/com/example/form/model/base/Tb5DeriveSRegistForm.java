7b3cc1c38b550aadeb75327f3e857828eb3dcb14
ToshiyukiFukuo <t.fukuo@gmail.com>
2025-08-17 14:41:54 +0900

    転生パターンに派生先追加

---
+++ examcom/src/main/java/com/example/form/model/base/Tb5DeriveSRegistForm.java
@@ -0,0 +1,47 @@
+package com.example.form.model.base;
+
+import java.util.List;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import jakarta.validation.Valid;
+import jp.co.golorp.emarf.process.BaseProcess;
+import jp.co.golorp.emarf.validation.IForm;
+
+/**
+ * 派生一覧登録フォーム
+ *
+ * @author emarfkrow
+ */
+public class Tb5DeriveSRegistForm implements IForm {
+
+    /** logger */
+    private static final Logger LOG = LoggerFactory.getLogger(Tb5DeriveRegistForm.class);
+
+    /** 派生登録フォームのリスト */
+    @Valid
+    private List<Tb5DeriveRegistForm> tb5DeriveGrid;
+
+    /**
+     * @return 派生登録フォームのリスト
+     */
+    public List<Tb5DeriveRegistForm> getTb5DeriveGrid() {
+        return tb5DeriveGrid;
+    }
+
+    /**
+     * @param p 派生登録フォームのリスト
+     */
+    public void setTb5DeriveGrid(final List<Tb5DeriveRegistForm> p) {
+        this.tb5DeriveGrid = p;
+    }
+
+    /** 関連チェック */
+    @Override
+    public void validate(final Map<String, String> errors, final BaseProcess baseProcess) {
+        LOG.debug("validate() not overridden in subclasses.");
+    }
+
+}
---
+++ examcom/src/main/java/com/example/form/model/base/Tb8ChoiceSRegistForm.java
@@ -0,0 +1,47 @@
+package com.example.form.model.base;
+
+import java.util.List;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import jakarta.validation.Valid;
+import jp.co.golorp.emarf.process.BaseProcess;
+import jp.co.golorp.emarf.validation.IForm;
+
+/**
+ * 選抜一覧登録フォーム
+ *
+ * @author emarfkrow
+ */
+public class Tb8ChoiceSRegistForm implements IForm {
+
+    /** logger */
+    private static final Logger LOG = LoggerFactory.getLogger(Tb8ChoiceRegistForm.class);
+
+    /** 選抜登録フォームのリスト */
+    @Valid
+    private List<Tb8ChoiceRegistForm> tb8ChoiceGrid;
+
+    /**
+     * @return 選抜登録フォームのリスト
+     */
+    public List<Tb8ChoiceRegistForm> getTb8ChoiceGrid() {
+        return tb8ChoiceGrid;
+    }
+
+    /**
+     * @param p 選抜登録フォームのリスト
+     */
+    public void setTb8ChoiceGrid(final List<Tb8ChoiceRegistForm> p) {
+        this.tb8ChoiceGrid = p;
+    }
+
+    /** 関連チェック */
+    @Override
+    public void validate(final Map<String, String> errors, final BaseProcess baseProcess) {
+        LOG.debug("validate() not overridden in subclasses.");
+    }
+
+}
